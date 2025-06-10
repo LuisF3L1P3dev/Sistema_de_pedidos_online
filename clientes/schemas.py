d52813023f1e556424c409b107bfee45b72ecc5d
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-06-10 14:29:36 -0300

    feat: add schemas, ClienteCreate e ClientePublic

--- clientes/schemas.py
+++ clientes/schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, EmailStr
 
-class ClientePublic(BaseModel):
-  id: id
+class ClienteBase(BaseModel):
   nome_cliente = str
   email: EmailStr
+
+class ClienteCreate(ClienteBase):
+  pass
+
+class ClientePublic(ClienteBase):
+  id: id
+
+  class Config:
+    orm_mode = True
